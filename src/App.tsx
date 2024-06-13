f9ac903ba7a519fb0e6056ea1ad36022a0cb7fb8
michael.nelms <michael.nelms@favordelivery.com>
2024-06-13 14:46:04 -0500

    [nt] mikes changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,60 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './App.css';
-import  Page  from "./Page";
-import Right_Arrow from './Vector_Arrow_Right.png';
-import Left_Arrow from './Vector_Arrow_Left.png';
-import Book from './book.png';
+import React, { useState, useEffect, useRef } from 'react'
+import './App.css'
+import Page from './Page'
+import Right_Arrow from './Vector_Arrow_Right.png'
+import Left_Arrow from './Vector_Arrow_Left.png'
+import Book from './book.png'
 
-const App : React.FC =() => {
-  const [pages, setPages] = useState<boolean[]>([true]);
-  return <div className = "Container">
-    <img src={Right_Arrow} id="right"></img>
-    <img src={Left_Arrow} id="left"></img>
-    <img src={Book} id = "book"></img>
-    {pages.map((visibility, index) => (
-      <Page key={index} visibility = {visibility} pageNum = {index} />
-    ))}
-  
-  </div>
+type Page = {
+  pageNum: number
 }
 
-export default App;
+const App: React.FC = () => {
+  const [currentPage, setCurrentPage] = useState(0)
+  const [pages, setPages] = useState<Page[]>([{
+    pageNum: 0
+  }])
+
+  console.log(currentPage, pages)
+
+  return (
+    <div className='Container'>
+      <button
+        id='right'
+        className='action-arrow'
+        onClick={() => {
+          // Create new page if that page doesnt exist
+          if (currentPage === pages.length - 1) {
+            setPages(prevState => [...prevState, { pageNum: pages.length }])
+          }
+          // Increment current page
+          setCurrentPage(prevState => prevState + 1)
+        }}
+      >
+        <img src={Right_Arrow}></img>
+      </button>
+
+      <button
+        id='left'
+        className='action-arrow'
+        onClick={() => {
+        }}
+      >
+        <img src={Left_Arrow}></img>
+      </button>
+
+      <button>
+        <img src={Book} id='book'></img>
+      </button>
+
+      {pages.map((page) => (
+        <div style={{ display: page.pageNum === currentPage ? 'block' : 'none' }}>
+          <Page key={page.pageNum} pageNum={page.pageNum} />
+        </div>
+      ))}
+
+    </div>
+  )
+}
+
+export default App
