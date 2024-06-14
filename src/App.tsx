74260579cd3904343affd1b505c9ee2cf10ea4d9
nnfvr <nathan.nguyen@favordelivery.com>
2024-06-14 11:30:31 -0500

    merge conflicts again

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ import Page from './Page'
 import Right_Arrow from './Vector_Arrow_Right.png'
 import Left_Arrow from './Vector_Arrow_Left.png'
 import Book from './book.png'
-<<<<<<< HEAD
 import TableOfContents from './TableOfContents'
-=======
->>>>>>> f9ac903 ([nt] mikes changes)
 
 type Page = {
   pageNum: number
@@ -15,16 +12,12 @@ type Page = {
 
 const App: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(0)
-<<<<<<< HEAD
   const [pageList, setPageList] = useState<number[]>([1]);
   const [pointerEvent, setPointerEvent] = useState<number>(0);
-=======
->>>>>>> f9ac903 ([nt] mikes changes)
   const [pages, setPages] = useState<Page[]>([{
     pageNum: 0
   }])
 
-<<<<<<< HEAD
   const setClicked = (index : number) => {
     setCurrentPage(index);
   }
@@ -51,23 +44,11 @@ const App: React.FC = () => {
         id='right'
         className='action-arrow'
         style={{pointerEvents: pointerEvent === -1 ? 'none' : 'auto'}}
-=======
-  console.log(currentPage, pages)
-
-  return (
-    <div className='Container'>
-      <button
-        id='right'
-        className='action-arrow'
->>>>>>> f9ac903 ([nt] mikes changes)
         onClick={() => {
           // Create new page if that page doesnt exist
           if (currentPage === pages.length - 1) {
             setPages(prevState => [...prevState, { pageNum: pages.length }])
-<<<<<<< HEAD
             setPageList(prevState => [...prevState, currentPage+2]);
-=======
->>>>>>> f9ac903 ([nt] mikes changes)
           }
           // Increment current page
           setCurrentPage(prevState => prevState + 1)
@@ -79,21 +60,16 @@ const App: React.FC = () => {
       <button
         id='left'
         className='action-arrow'
-<<<<<<< HEAD
         style={{pointerEvents: pointerEvent === -1 ? 'none' : 'auto'}}
         onClick={() => {
           if (currentPage !== 0) {
             setCurrentPage(prevState => prevState-1);
           }
-=======
-        onClick={() => {
->>>>>>> f9ac903 ([nt] mikes changes)
         }}
       >
         <img src={Left_Arrow}></img>
       </button>
 
-<<<<<<< HEAD
       <button
         id='book'
         onClick={displayTable}
@@ -105,15 +81,6 @@ const App: React.FC = () => {
       {pages.map((page) => (
         <div style={{ display: page.pageNum === currentPage ? 'block' : 'none', pointerEvents : pointerEvent === -1 ? 'none' : 'auto'}}>
           <Page key={page.pageNum} pageNum={page.pageNum}/>
-=======
-      <button>
-        <img src={Book} id='book'></img>
-      </button>
-
-      {pages.map((page) => (
-        <div style={{ display: page.pageNum === currentPage ? 'block' : 'none' }}>
-          <Page key={page.pageNum} pageNum={page.pageNum} />
->>>>>>> f9ac903 ([nt] mikes changes)
         </div>
       ))}
 
